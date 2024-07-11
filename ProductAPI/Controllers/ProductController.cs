a6b53c516a4afc30945e78a6b047c8d3339b5ff8
Danna Chen <dannachen97@gmail.com>
2024-07-11 02:58:19 -0400

    Add ProductController

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationCore.Entity;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> AddProduct(Product obj) {
+            return Ok(obj);
+        }
+    }
+}
